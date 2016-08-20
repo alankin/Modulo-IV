e7e1202b0cfa1eddb47c71c159a580fa082535a7
QUIÑONES ALVAREZ ALAIN <ALANKIN@macbook-pro-de-quinones.local>
2016-08-20 16:08:38 -0400

    Initial Commit

---
+++ CComunities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CComunities
+//
+//  Created by QUIÑONES ALVAREZ ALAIN on 20/8/16.
+//  Copyright © 2016 QUIÑONES ALVAREZ ALAIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
